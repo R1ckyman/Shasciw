924afb17c911b573d46a00dfdd7e1a6cf78b62b9
Ricky <ricky.rfr@gmail.com>
2019-02-11 20:26:00 +0100

    Now the bullet damage will be the player's damage

--- source/Bullet.cpp
+++ source/Bullet.cpp
@@ -1,15 +1,15 @@
 #include "Bullet.h"
 
-Bullet::Bullet(unsigned x, unsigned y, Dir dir, unsigned player){
-  this->x = x;
-  this->y = y;
-  this->dir = dir;
-  this->damage = 1;
-  this->player = player;
+Bullet::Bullet(unsigned x, unsigned y, Dir dir, unsigned player, unsigned damage) {
+	this->x = x;
+	this->y = y;
+	this->dir = dir;
+	this->damage = damage;
+	this->player = player;
 }
-void Bullet::setX(unsigned x){
-  this->x = x;
+void Bullet::setX(unsigned x) {
+	this->x = x;
 }
-void Bullet::setY(unsigned y){
-  this->y = y;
+void Bullet::setY(unsigned y) {
+	this->y = y;
 }
--- source/libs/Bullet.h
+++ source/libs/Bullet.h
@@ -1,30 +1,30 @@
 #ifndef _Bullet_H_
 #define _Bullet_H_
 
-enum Dir{
-  DIR_UP,
-  DIR_DOWN,
-  DIR_RIGHT,
-  DIR_LEFT,
-  DIR_NULL
+enum Dir {
+	DIR_UP,
+	DIR_DOWN,
+	DIR_RIGHT,
+	DIR_LEFT,
+	DIR_NULL
 };
 
-class Bullet{
-  private:
-    Dir dir;
-    unsigned x;
-    unsigned y;
-    unsigned damage;
-    unsigned player;
-  public:
-    Bullet(unsigned x, unsigned y, Dir dir, unsigned player);
-    unsigned getX() const { return x; };
-    unsigned getY() const { return y; };
-    unsigned getDir() const { return dir; };
-    unsigned getDamage() const { return damage; };
-    unsigned getPlayer() const { return player; };
-    void setX(unsigned x);
-    void setY(unsigned y);
+class Bullet {
+private:
+	Dir dir;
+	unsigned x;
+	unsigned y;
+	unsigned damage;
+	unsigned player;
+public:
+	Bullet(unsigned x, unsigned y, Dir dir, unsigned player, unsigned damage);
+	unsigned getX() const { return x; };
+	unsigned getY() const { return y; };
+	unsigned getDir() const { return dir; };
+	unsigned getDamage() const { return damage; };
+	unsigned getPlayer() const { return player; };
+	void setX(unsigned x);
+	void setY(unsigned y);
 };
 
 #endif
