bb279d9f9b90975cc44f7ba797b7e7a6edc8630f
Ricky <ricky.rfr@gmail.com>
2019-02-11 20:24:59 +0100

    Added damage variable with getter and setter and added getObject and removeObject methods

--- source/Player.cpp
+++ source/Player.cpp
@@ -1,92 +1,108 @@
 #include "Player.h"
 #include <cstring>
 
-Player::Player(char name[9], int x, int y, unsigned id){
-  strcpy(this->name, name);
-  letter = 'A';
-  this->x = x;
-  this->y = y;
-  healt = 3;
-  moves = 3;
-  maxMoves = 3;
-  last_move = DIR_NULL;
-  this-> id = id;
-  stats_changed = false;
-  inventory_changed = false;
-  inventory.push_back(0);
+Player::Player(char name[9], int x, int y, unsigned id) {
+	strcpy(this->name, name);
+	letter = 'A';
+	this->x = x;
+	this->y = y;
+	healt = 3;
+	moves = 3;
+	maxMoves = 3;
+	damage = 1;
+	last_move = DIR_NULL;
+	this->id = id;
+	stats_changed = false;
+	inventory_changed = false;
+	inventory.push_back(0);
 }
-void Player::setName(char name[9]){
-  strcpy(this->name, name);
+void Player::setName(char name[9]) {
+	strcpy(this->name, name);
 }
-void Player::setLetter(char letter){
-  this->letter = letter;
+void Player::setLetter(char letter) {
+	this->letter = letter;
 }
-void Player::setHealt(int healt){
-  this->healt = healt;
+void Player::setHealt(int healt) {
+	this->healt = healt;
 }
-void Player::setX(unsigned x){
-  this->x = x;
+void Player::setX(unsigned x) {
+	this->x = x;
 }
-void Player::setY(unsigned y){
-  this->y = y;
+void Player::setY(unsigned y) {
+	this->y = y;
 }
-void Player::setMoves(unsigned moves){
-  this->moves = moves;
+void Player::setMoves(unsigned moves) {
+	this->moves = moves;
 }
-void Player::setMaxMoves(unsigned maxMoves){
-  this->maxMoves = maxMoves;
+void Player::setMaxMoves(unsigned maxMoves) {
+	this->maxMoves = maxMoves;
 }
-void Player::setLastMove(Dir dir){
-  last_move = dir;
+void Player::setDamage(unsigned damage) {
+	this->damage = damage;
 }
-void Player::setStatsChanged(bool changed){
-  stats_changed = changed;
+void Player::setLastMove(Dir dir) {
+	last_move = dir;
 }
-void Player::setInventoryChanged(bool changed){
-  inventory_changed = changed;
+void Player::setStatsChanged(bool changed) {
+	stats_changed = changed;
 }
-bool Player::addObject(unsigned object){
-  if(inventory.size() < INVENTORY){
-    inventory.push_back(object);
-    return true;
-  }
-  return false;
+void Player::setInventoryChanged(bool changed) {
+	inventory_changed = changed;
 }
-void Player::printInfo() const{
-  printf("\x1b[9;60H->Moves: %u",moves+1);
+bool Player::addObject(unsigned object) {
+	if (inventory.size() < INVENTORY) {
+		inventory.push_back(object);
+		return true;
+	}
+	return false;
 }
-void Player::printStats() const{
-  printf("\x1b[7;1H| %s\'s healt: %d/3 \x1b[7;24H|",name,healt);
-  printf("\x1b[9;1H| %s\'s inventory \x1b[9;24H|",name);
+void Player::printInfo() const {
+	printf("\x1b[9;60H->Moves: %u", moves + 1);
 }
-void Player::printInventory(unsigned index) const{
-  unsigned i;
-  for(i=0;i<INVENTORY;i++){
-    printf("\x1b[%d;1H", 11+i*2);
-    if(i < getInventorySize()){
-      if(i == index) printf("\x1b[31m");
-      switch(inventory[i]){
-        case 0:
-          printf("-Add 1 move      ");
-          break;
-        case 1:
-          printf("-Add 2 moves     ");
-          break;
-        case 2:
-          printf("-Add 3 moves     ");
-          break;
-        case 3:
-          printf("-[P] Add 1 move  ");
-          break;
-        case 4:
-          printf("-Add 1 damage    ");
-          break;
-        default:
-          printf("-Undefined object");
-          break;
-      }
-      if(i == index) printf("\x1b[0m");
-    }
-    else printf("                 ");
-  }
+void Player::printStats() const {
+	printf("\x1b[7;1H| %s\'s healt: %d/3 \x1b[7;24H|", name, healt);
+	printf("\x1b[9;1H| %s\'s inventory \x1b[9;24H|", name);
+}
+void Player::removeObject(unsigned object) {
+	for (unsigned i = 0;i < inventory.size();i++)
+		if (object == inventory[i])
+			inventory.erase(inventory.begin() + i);
+}
+
+unsigned Player::getObject(unsigned index) {
+	unsigned object = inventory[index];
+	removeObject(object);
+
+	return object;
+}
+void Player::printInventory(unsigned index) const {
+	unsigned i;
+	for (i = 0;i < INVENTORY;i++) {
+		printf("\x1b[%d;1H", 11 + i * 2);
+		if (i < getInventorySize()) {
+			if (i == index) printf("\x1b[31m");
+			switch (inventory[i]) {
+			case 0:
+				printf("-Add 1 move      ");
+				break;
+			case 1:
+				printf("-Add 2 moves     ");
+				break;
+			case 2:
+				printf("-Add 3 moves     ");
+				break;
+			case 3:
+				printf("-[P] Add 1 move  ");
+				break;
+			case 4:
+				printf("-Add 1 damage    ");
+				break;
+			default:
+				printf("-Undefined object");
+				break;
+			}
+			if (i == index) printf("\x1b[0m");
+		}
+		else printf("                 ");
+	}
 }
--- source/libs/Player.h
+++ source/libs/Player.h
@@ -7,49 +7,53 @@
 
 const unsigned INVENTORY = 10;
 
-class Player{
-  private:
-    unsigned id;
-    char name[8];
-    char letter;
-    int healt;
-    unsigned x;
-    unsigned y;
-    std::vector<unsigned> inventory;
-    unsigned moves;
-    unsigned maxMoves;
-    Dir last_move;
-    bool stats_changed;
-    bool inventory_changed;
-  public:
-    Player(char name[8], int x, int y, unsigned id);
-    char getLetter() const { return letter; };
-    int getHealt() const { return healt; };
-    int getObject(unsigned i) const { return inventory[i]; };
-    unsigned getX() const { return x; };
-    unsigned getY() const { return y; };
-    char getName(unsigned position) const { return name[position]; };
-    unsigned getMoves() const { return moves; };
-    unsigned getMaxMoves() const { return maxMoves; };
-    Dir getLastMove() const { return last_move; };
-    unsigned getId() const { return id; };
-    bool getStatsChanged() const { return stats_changed; };
-    bool getInventoryChanged() const { return inventory_changed; };
-    unsigned getInventorySize() const { return inventory.size(); };
-    void setName(char name[8]);
-    void setLetter(char letter);
-    void setHealt(int healt);
-    void setX(unsigned x);
-    void setY(unsigned y);
-    void setMoves(unsigned moves);
-    void setMaxMoves(unsigned maxMoves);
-    void setLastMove(Dir last_move);
-    bool addObject(unsigned object);
-    void printInfo() const;
-    void printStats() const;
-    void printInventory(unsigned index) const;
-    void setStatsChanged(bool changed);
-    void setInventoryChanged(bool changed);
+class Player {
+private:
+	unsigned id;
+	char name[8];
+	char letter;
+	int healt;
+	unsigned x;
+	unsigned y;
+	std::vector<unsigned> inventory;
+	unsigned moves;
+	unsigned maxMoves;
+	unsigned damage;
+	Dir last_move;
+	bool stats_changed;
+	bool inventory_changed;
+	void removeObject(unsigned object);
+public:
+	Player(char name[8], int x, int y, unsigned id);
+	char getLetter() const { return letter; };
+	int getHealt() const { return healt; };
+	unsigned getObject(unsigned index);
+	unsigned getX() const { return x; };
+	unsigned getY() const { return y; };
+	char getName(unsigned position) const { return name[position]; };
+	unsigned getMoves() const { return moves; };
+	unsigned getMaxMoves() const { return maxMoves; };
+	Dir getLastMove() const { return last_move; };
+	unsigned getId() const { return id; };
+	bool getStatsChanged() const { return stats_changed; };
+	bool getInventoryChanged() const { return inventory_changed; };
+	unsigned getInventorySize() const { return inventory.size(); };
+	unsigned getDamage() const { return damage; };
+	void setName(char name[8]);
+	void setLetter(char letter);
+	void setHealt(int healt);
+	void setX(unsigned x);
+	void setY(unsigned y);
+	void setMoves(unsigned moves);
+	void setMaxMoves(unsigned maxMoves);
+	void setDamage(unsigned damage);
+	void setLastMove(Dir last_move);
+	bool addObject(unsigned object);
+	void printInfo() const;
+	void printStats() const;
+	void printInventory(unsigned index = INVENTORY + 1) const;
+	void setStatsChanged(bool changed);
+	void setInventoryChanged(bool changed);
 };
 
 #endif
