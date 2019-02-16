2e0ed525472e1b3bb80fe7700fae3687d86eea98
Ricky <ricky.rfr@gmail.com>
2019-02-16 13:10:40 +0100

    Added keys constants, and input for more players

--- source/libs/Input.h
+++ source/libs/Input.h
@@ -3,9 +3,33 @@
 
 #include <switch.h>
 
+#define ACTION_1		KEY_SL
+#define ACTION_2		KEY_SR
+#define EXIT			(KEY_PLUS	| KEY_MINUS)
+#define UP				(KEY_DRIGHT	| KEY_Y)
+#define DOWN			(KEY_DLEFT	| KEY_A)
+#define LEFT			(KEY_DUP	| KEY_B)
+#define RIGHT			(KEY_DDOWN	| KEY_X)
+#define SPECIAL_1		(KEY_L		| KEY_R)
+#define SPECIAL_2		(KEY_ZL		| KEY_ZR)
 class Input {
 public:
-	static u32 getInputDown() { return hidKeysDown(CONTROLLER_P1_AUTO); };
+	static u64 getInputDown(unsigned player) { 
+		switch (player) {
+		case 0:
+			return hidKeysDown(CONTROLLER_PLAYER_1);
+			break;
+		case 1:
+			return hidKeysDown(CONTROLLER_PLAYER_2);
+			break;
+		case 2:
+			return hidKeysDown(CONTROLLER_PLAYER_3);
+			break;
+		default:
+			return hidKeysDown(CONTROLLER_PLAYER_4);
+			break;
+		}
+	};
 };
 
 #endif
