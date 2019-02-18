74d53624331b6a8db00057c94ea8b0927dc8f816
Ricky <ricky.rfr@gmail.com>
2019-02-18 20:45:34 +0100

    Added settings menu which allows to choose players and controls

--- source/main.cpp
+++ source/main.cpp
@@ -1,9 +1,11 @@
+//#define DEBUG
+
 #include "Keyboard.h"
 #include "Map.h"
 
 void printControls() {
 	consoleClear();
-	printf(ANSI_COLOR_BOLDYELLOW "\x1b[3;25HReturn to main menu (+)" ANSI_COLOR_RESET);
+	printf(ANSI_COLOR_BOLDYELLOW "\x1b[3;27HReturn(+, -)" ANSI_COLOR_RESET);
 	printf(ANSI_COLOR_BOLDCYAN "\x1b[7;1H[Input] - Left Joycon (Horizontal)" ANSI_COLOR_RESET);
 	printf("\x1b[9;1H[Keyboard]");
 	printf("\x1b[11;3H-%c, %c, %c, %c, to control the keyboard", 24, 25, 26, 27);
@@ -21,38 +23,72 @@ void printControls() {
 	printf("\x1b[36;3H-SR to select action (shoot/open) and X,B,A,Y to select the direction");
 	printf("\x1b[38;3H-SR to use an object and SR to open inventory");
 }
+void printSettings(unsigned index, unsigned players, unsigned joycons) {
+	if (index == 0) printf(ANSI_COLOR_BOLDYELLOW);
+	else printf(ANSI_COLOR_YELLOW);
+	printf("\x1b[3;27HSave and return (+, -)" ANSI_COLOR_RESET);
+	printf(ANSI_COLOR_CYAN "\x1b[5;32HPlayers: %d" ANSI_COLOR_RESET, players);
+	if (index == 1) printf(ANSI_COLOR_BOLDGREEN);
+	else printf(ANSI_COLOR_GREEN);
+	printf("\x1b[7;27HAdd player" ANSI_COLOR_RESET);
+	if (index == 2) printf(ANSI_COLOR_BOLDRED);
+	else printf(ANSI_COLOR_RED);
+	printf("\x1b[9;27HRemove player" ANSI_COLOR_RESET);
+	if (index == 3) printf(ANSI_COLOR_BOLDMAGENTA);
+	else printf(ANSI_COLOR_MAGENTA);
+	printf("\x1b[12;27HChange controls" ANSI_COLOR_RESET);
+	switch (joycons)
+	{
+	case 0:
+		printf(ANSI_COLOR_WHITE "\x1b[14;32H2 joycons for all  " ANSI_COLOR_RESET);
+		break;
+	case 1:
+		printf(ANSI_COLOR_WHITE "\x1b[14;32H1 joycon per player" ANSI_COLOR_RESET);
+		break;
+	default:
+		printf(ANSI_COLOR_WHITE "\x1b[14;32H1 joycon for all   " ANSI_COLOR_RESET);
+		break;
+	}
+}
 void printMenu(unsigned index) {
 	if (index == 0) printf(ANSI_COLOR_BOLDRED);
 	else printf(ANSI_COLOR_RED);
-	printf("\x1b[4;27HQuickplay" ANSI_COLOR_RESET);
+	printf("\x1b[5;27HQuickplay" ANSI_COLOR_RESET);
 	if (index == 1) printf(ANSI_COLOR_BOLDYELLOW);
 	else printf(ANSI_COLOR_YELLOW);
-	printf("\x1b[8;27HName selector" ANSI_COLOR_RESET);
+	printf("\x1b[9;27HName selector" ANSI_COLOR_RESET);
 	if (index == 2) printf(ANSI_COLOR_BOLDGREEN);
 	else printf(ANSI_COLOR_GREEN);
-	printf("\x1b[12;27HLoad saved game (not yet)" ANSI_COLOR_RESET);
-	if (index == 3) printf(ANSI_COLOR_BOLDCYAN);
-	else printf(ANSI_COLOR_CYAN);
-	printf("\x1b[16;27HControls menu" ANSI_COLOR_RESET);
-	if (index == 4) printf(ANSI_COLOR_BOLDMAGENTA);
+	printf("\x1b[13;27HLoad saved game (not yet)" ANSI_COLOR_RESET);
+	if (index == 3) printf(ANSI_COLOR_BOLDMAGENTA);
 	else printf(ANSI_COLOR_MAGENTA);
-	printf("\x1b[20;27HReturn to Homebrew menu (+)" ANSI_COLOR_RESET);
+	printf("\x1b[17;27HSettings" ANSI_COLOR_RESET);
+	if (index == 4) printf(ANSI_COLOR_BOLDCYAN);
+	else printf(ANSI_COLOR_CYAN);
+	printf("\x1b[21;27HControls menu" ANSI_COLOR_RESET);
+	if (index == 5) printf(ANSI_COLOR_WHITE);
+	printf("\x1b[25;27HReturn to Homebrew menu (+, -)" ANSI_COLOR_RESET);
 }
-void printWinner(unsigned player, const Map &map) {
+void printWinner(const Player &player) {
 	unsigned i;
 
-	switch (player)
-	{
+	switch (player.getId()){
 	case 0:
 		printf(ANSI_COLOR_BOLDYELLOW);
 		break;
-	default:
+	case 1:
 		printf(ANSI_COLOR_BOLDGREEN);
 		break;
+	case 2:
+		printf(ANSI_COLOR_BOLDMAGENTA);
+		break;
+	default:
+		printf(ANSI_COLOR_BOLDCYAN);
+		break;
 	}
-	printf("\x1b[25;30H--- ");
+	printf("\x1b[29;30H--- ");
 	for (i = 0;i < 8;i++) {
-		printf("%c", map.getPlayer(player).getName(i));
+		printf("%c", player.getName(i));
 	}
 	printf(" won---" ANSI_COLOR_RESET);
 
@@ -99,20 +135,22 @@ int main(int argc, char **argv)
 
 	char letter;
 
-	int i;
 	int name_letters = 0;
 	int special = 0;
 
+	unsigned i;
 	unsigned state = 0;
 	unsigned inventory_index = 0;
 	unsigned menu_index = 0;
+	unsigned settings_index = 0;
 	unsigned player_index = 0;
+	unsigned joycons_controls = 0; 
 	
 	Map map;
 	Keyboard keyboard(keyboard_1);
 	Player temp_player = map.getPlayer(0);
 
-	unsigned players = map.getPlayers();
+	unsigned players = map.getPlayers()+1;
 
 	consoleInit(NULL);
 
@@ -125,9 +163,28 @@ int main(int argc, char **argv)
 		hidScanInput();
 
 		u64 kDown = Input::getInputDown(0);
-		u64 kDown_P2 = Input::getInputDown(1);
-		u64 kDown_P3 = Input::getInputDown(0);
-		u64 kDown_P4 = Input::getInputDown(1);
+		u64 kDown_P2;
+		u64 kDown_P3;
+		u64 kDown_P4;
+
+		switch (joycons_controls)
+		{
+		case 0:
+			kDown_P2 = Input::getInputDown(1);
+			kDown_P3 = Input::getInputDown(0);
+			kDown_P4 = Input::getInputDown(1);
+			break;
+		case 1:
+			kDown_P2 = Input::getInputDown(1);
+			kDown_P3 = Input::getInputDown(2);
+			kDown_P4 = Input::getInputDown(3);
+			break;
+		default:
+			kDown_P2 = Input::getInputDown(0);
+			kDown_P3 = Input::getInputDown(0);
+			kDown_P4 = Input::getInputDown(0);
+			break;
+		}
 
 		u64 key;
 		switch (player_index){
@@ -148,7 +205,7 @@ int main(int argc, char **argv)
 		switch (state) {
 		case 0: // Main menu
 			if ((kDown | kDown_P2 | kDown_P3 | kDown_P4) & DOWN) {
-				if (menu_index < 4) menu_index++;
+				if (menu_index < 5) menu_index++;
 				printMenu(menu_index);
 			}
 			if ((kDown | kDown_P2 | kDown_P3 | kDown_P4) & UP) {
@@ -160,6 +217,7 @@ int main(int argc, char **argv)
 				{
 				case 0: // Quickplay
 					consoleClear();
+					map.setPlayers(players);
 					map.printMapFull(map.getPlayer(player_index));
 					state = 4;
 					break;
@@ -170,7 +228,12 @@ int main(int argc, char **argv)
 				case 2: // Load Game
 					printMenu(0);
 					break;
-				case 3: // Print Controls
+				case 3: // Settings
+					consoleClear();
+					printSettings(settings_index, players, joycons_controls);
+					state = 5;
+					break;
+				case 4: // Print Controls
 					state = 1;
 					printControls();
 					break;
@@ -202,7 +265,7 @@ int main(int argc, char **argv)
 					map.modifyPlayer(player_index, temp_player);
 					keyboard.setCaps(true);
 					keyboard.setIndex(0);
-					if (player_index < players) player_index++;
+					if (player_index < players-1) player_index++;
 					else {
 						player_index = 0;
 						state = 3; // Letter selector
@@ -245,13 +308,14 @@ int main(int argc, char **argv)
 				map.modifyPlayer(player_index, temp_player);
 				keyboard.setCaps(true);
 				keyboard.setIndex(0);
-				if (player_index < players) {
+				if (player_index < players-1) {
 					player_index++;
 					consoleClear();
 				}
 				else {
 					player_index = 0;
 					consoleClear();
+					map.setPlayers(players);
 					map.printMapFull(map.getPlayer(player_index));
 					// Game process
 					state = 4;
@@ -260,11 +324,19 @@ int main(int argc, char **argv)
 			else keyboard.printKeyboard();
 			break;
 		case 4: // Game process
-			// If all players are alive
-			if (map.getPlayer(player_index).getHealt() > 0) {
-				// Player turn
+			#ifdef DEBUG
+				printf(ANSI_COLOR_BOLDRED "\x1b[2;2HID: %d | Healt: %d 1 > 4?" ANSI_COLOR_RESET, map.getPlayer(player_index).getId(), map.getPlayer(player_index).getHealt());
+				printf(ANSI_COLOR_BOLDRED "\x1b[3;2HPLAYERS: %d | INDEX: %u" ANSI_COLOR_RESET, map.getPlayers(), player_index);
+			#endif // DEBUG
+			// Player turn
+			if (map.getPlayers() > 0) {
+				for (i = 0; i <= map.getPlayers();i++) {
+					if (map.getPlayer(i).getHealt() <= 0) {
+						if (player_index > 0 && i < map.getPlayers()) player_index--;
+						map.removePlayer(i);
+					}
+				}
 				temp_player = map.getPlayer(player_index);
-
 				if (map.processGame(temp_player, special, inventory_index, key)) {
 					if (temp_player.getMoves() > 0) {
 						temp_player.setMoves(temp_player.getMoves() - 1);
@@ -280,12 +352,8 @@ int main(int argc, char **argv)
 						temp_player.setLastMove(DIR_NULL);
 						temp_player.setInventoryChanged(true);
 						map.modifyPlayer(player_index, temp_player);
-						if (player_index < players) {
-							player_index++;
-						}
-						else {
-							player_index = 0;
-						}
+						if (player_index < map.getPlayers()) player_index++;
+						else player_index = 0;
 						map.getPlayer(player_index).printStats();
 						map.getPlayer(player_index).printInfo();
 						map.getPlayer(player_index).printInventory();
@@ -295,11 +363,43 @@ int main(int argc, char **argv)
 				else map.modifyPlayer(player_index, temp_player);
 			}
 			else {
+				temp_player = map.getPlayer(0);
 				restart(map);
-				if (map.getPlayer(0).getHealt() <= 0) printWinner(1, map);
-				else printWinner(0, map);
-
+				printWinner(temp_player);
 				state = 0;
+				player_index = 0;
+			}
+			break;
+		case 5: // Settings menu
+			if ((kDown | kDown_P2 | kDown_P3 | kDown_P4) & DOWN) {
+				if (settings_index < 3) settings_index++;
+				printSettings(settings_index, players, joycons_controls);
+			}
+			if ((kDown | kDown_P2 | kDown_P3 | kDown_P4) & UP) {
+				if (settings_index > 0) settings_index--;
+				printSettings(settings_index, players, joycons_controls);
+			}
+			if ((kDown | kDown_P2 | kDown_P3 | kDown_P4) & ACTION_1) {
+				switch (settings_index) {
+				case 0:
+					state = 0;
+					consoleClear();
+					printMenu(0);
+					break;
+				case 1:
+					if(players < 4) players++;
+					printSettings(settings_index, players, joycons_controls);
+					break;
+				case 2:
+					if (players > 2) players--;
+					printSettings(settings_index, players, joycons_controls);
+					break;
+				default:
+					if (joycons_controls < 2) joycons_controls++;
+					else joycons_controls = 0;
+					printSettings(settings_index, players, joycons_controls);
+					break;
+				}
 			}
 			break;
 		default:
@@ -327,6 +427,11 @@ int main(int argc, char **argv)
 				// Save Game
 				state = 0;
 				restart(map);
+				break;
+			case 5:
+				state = 0;
+				consoleClear();
+				printMenu(0);
 				break;
 			default:
 				active = false;
